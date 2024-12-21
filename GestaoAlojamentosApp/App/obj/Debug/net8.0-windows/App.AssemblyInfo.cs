59e8ced7d3f22930f6802c119fcad847ef06237b
Tiago Vale <tiagodanielvale@gmail.com>
2024-12-21 16:09:58 +0000

    delete doc

--- GestaoAlojamentosApp/App/obj/Debug/net8.0-windows/App.AssemblyInfo.cs
+++ GestaoAlojamentosApp/App/obj/Debug/net8.0-windows/App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35baa2431c11b9676aec841cee1c0a35a8834c97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a80d20149375871cb2e0d79f5b13f1fcade9a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
